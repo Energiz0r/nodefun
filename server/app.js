6898189bb2268bd3897bd4e5a5dc37740fb3ef54
Tommy <tommykri@gmail.com>
2014-05-18 21:55:30 +0200

    Prover igjen

---
+++ server/app.js
@@ -0,0 +1,4 @@
+console.log('yo');
+
+var msgs = require("./msgs.js")
+console.log(msgs.first);
---
+++ server/msgs.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Energiz0r on 18.05.2014.
+ */
+exports.first = "This is the first message";
